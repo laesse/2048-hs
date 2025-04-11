9d4c1b9478e672049931e36bb6221cbdebbdcacb
laesse <lars.volkheimer@gmx.ch>
2025-04-11 10:14:40 +0200

    get random placement & game over working
    Note compiled version works because we set buffering to NoBuffering! in
    ghci this is the default

--- src/main.hs
+++ src/main.hs
@@ -1,9 +1,12 @@
 module Main where
 
-import Data.Maybe (fromMaybe, isNothing)
-import System.Random (StdGen)
+import Control.Monad.IO.Class (MonadIO)
+import Data.List (find)
+import Data.Maybe (isJust, isNothing)
+import System.IO
+import System.Random (Random (..), getStdRandom)
 
-type GameState = [[Int]]
+type GameState = [[Integer]]
 
 getGameStateStr :: GameState -> String
 getGameStateStr state = getGameStateStr_ state ""
@@ -13,15 +16,14 @@ getGameStateStr state = getGameStateStr_ state ""
     [] -> acc
     row : rows -> getRow_ row ++ "\n" ++ getGameStateStr_ rows acc
    where
-    getRow_ :: [Int] -> String
+    getRow_ :: [Integer] -> String
     getRow_ = concatMap show
 
 data Input = InUp | InLeft | InRight | InDown deriving (Show, Eq)
 
 getInput :: IO Input
 getInput = do
-  putStr "in: "
-  rawIn <- getChar
+  rawIn <- putStr "in: " >>= const getChar
   let val =
         ( case rawIn of
             'w' -> Just InUp
@@ -34,22 +36,66 @@ getInput = do
 
   maybe getInput return val
 
-makeMove :: Input -> GameState -> GameState
-makeMove input state = state
+putNewTile :: (Int, Int, Integer) -> GameState -> GameState
+putNewTile (y, x, value) = mapi (\row rowi -> mapi (\val vali -> if x == vali && y == rowi then value else val) row)
+
+makeMove :: (MonadIO m) => Input -> GameState -> m GameState
+makeMove input state = do
+  let state2 = processInput input state
+  newTileLoc <- randomPlace state
+  let nextState = putNewTile newTileLoc state2
+  return nextState
+
+processInput :: Input -> GameState -> GameState
+processInput input state = state
+
+gameStateGet :: GameState -> (Int, Int) -> Integer
+gameStateGet state (x, y) = (state !! x) !! y
+
+getRandomLoc :: (MonadIO m) => [[Integer]] -> m (Int, Int)
+getRandomLoc state = do
+  x <- randRange 0 3
+  y <- randRange 0 3
+  let val = gameStateGet state (x, y)
+  if val == 0
+    then return (x, y)
+    else getRandomLoc state
+
+randomPlace :: (MonadIO m) => GameState -> m (Int, Int, Integer)
+randomPlace state = do
+  (x, y) <- getRandomLoc state
+  val <- randRange 1 2
+  return (x, y, val * 2)
+
+randRange :: (MonadIO m, Random a) => a -> a -> m a
+randRange lo hi = getStdRandom (randomR (lo, hi))
 
 mainGameLoop :: GameState -> IO ()
 mainGameLoop state = do
   putStrLn $ getGameStateStr state
   inputValue <- getInput
   print $ show inputValue
+  nextState <- makeMove inputValue state
 
-  mainGameLoop $ makeMove inputValue state
+  let gameOver = isNothing $ find (isJust . find (== 0)) nextState
+
+  if gameOver
+    then putStrLn "game over"
+    else mainGameLoop nextState
 
 initialState :: GameState
 initialState = replicate 4 $ replicate 4 0
 
+mapi :: (a -> Int -> a) -> [a] -> [a]
+mapi mapFn lst = mapi_ lst 0
+ where
+  mapi_ [] _ = []
+  mapi_ (x : xs) i = mapFn x i : mapi_ xs (i + 1)
+
 main :: IO ()
 main = do
+  hSetBuffering stdin NoBuffering
+  hSetBuffering stdout NoBuffering
   putStrLn "Welcome to 2048-hs"
   mainGameLoop initialState
   putStrLn "Thanks for playing"
